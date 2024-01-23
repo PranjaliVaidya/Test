6a18f089709e276e8465e095d3634c688b7ecdc4
Pranjali Vaidya <91623757+PranjaliVaidya@users.noreply.github.com>
2024-01-23 16:42:22 +0530

    added Yoo

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry=input("Are you hungry?")
-if(hungry=="yes"):
+if(hungry=="yes" and hungry=="yo") :
     print("Eat Pizza")
-
+    print("Eat Samosa")
+    print("Eat Pavbhaji")
 else:
     print("Do homework")
